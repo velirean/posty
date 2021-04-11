908885660b3e5f7831ad81e4b4d8511590f66964
velirean <47877809+velirean@users.noreply.github.com>
2021-04-11 11:09:03 -0500

    Dashboard route and basic blade

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -32,6 +32,6 @@ class RegisterController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
-        return redirect()->route('posts');
+        return redirect()->route('dashboard');
     }
 }
