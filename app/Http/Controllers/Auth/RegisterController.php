94b6290f3a22a96db28ab9902ad4f84292789048
velirean <47877809+velirean@users.noreply.github.com>
2021-04-11 11:31:19 -0500

    Login after sign up and personalized navbar

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
@@ -25,12 +26,14 @@ class RegisterController extends Controller
             'password_confirmation' => 'required',
         ]);
 
-        User::create([
+        $user = User::create([
             'name' => $request->name,
             'username' => $request->username,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+
+        Auth::login($user);
 
         return redirect()->route('dashboard');
     }
